a382fc6c317ca25721341ddde2976225b3b9fb03
André <AndreR@ppedv.de>
2024-09-23 11:25:56 +0200

    Add Singleton Logger and integrate into Program.cs
    A new Visual Studio solution file `HalloSingelton.sln` was created with configuration settings for Visual Studio Version 17. It includes a project named "HalloSingelton" with its corresponding project file `HalloSingelton\HalloSingelton.csproj`.
    
    A new project file `HalloSingelton.csproj` was added, specifying the project as an executable targeting .NET 8.0, with implicit usings and nullable reference types enabled.
    
    A new class `Logger` was added in `Logger.cs`. This class implements the Singleton pattern to ensure only one instance of the logger exists. It includes methods for logging informational and warning messages.
    
    The `Program.cs` file was updated to use the `Logger` class. It includes a parallel loop that logs messages using the singleton instance of the `Logger`.

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,33 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static Logger _instance = null;
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    lock (typeof(Logger))
+                        _instance ??= new Logger();
+
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            LogWarn("Logger created");
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.WriteLine($"Info [{DateTime.Now:g}]: {message}");
+        }
+
+        public void LogWarn(string message)
+        {
+            Console.WriteLine($"Warn [{DateTime.Now:g}]: {message}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,13 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+
+//for (int i = 0; i < 10_000_000; i++)
+Parallel.For(0, 10, i =>
+{
+    Logger.Instance.LogInfo($"Hello Logger {i}");
+});
+
+
+
