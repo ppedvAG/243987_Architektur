6b41361ad980d6133882c105644b5c8c6a489cab
André <AndreR@ppedv.de>
2024-09-23 12:21:14 +0200

    Add Decorator pattern implementation and demo
    Introduce a new Visual Studio solution file (`DecoPattern.sln`) for the project. Add a new C# file (`Deco.cs`) implementing the Decorator design pattern with `IComponent` interface, concrete classes `Pizza` and `Brot`, and abstract class `Deco`. Include concrete decorators `Salami` and `Käse`.
    
    Add a new project file (`DecoPattern.csproj`) specifying project type, target framework (`net8.0`), and enabling implicit usings and nullable reference types.
    
    Update `Program.cs` to demonstrate the decorator pattern with a console application that creates a `Brot` object decorated with `Salami` and `Käse`, prints its name and price, and serializes it to JSON.

---
+++ DecoPattern/DecoPattern/Deco.cs
@@ -0,0 +1,57 @@
+﻿namespace DecoPattern
+{
+    internal interface IComponent
+    {
+        string Name { get; }
+        decimal Price { get; }
+    }
+
+    public class Pizza : IComponent
+    {
+        public string Name => "Pizza";
+
+        public decimal Price => 8.6m;
+    }
+
+    public class Brot : IComponent
+    {
+        public string Name => "Brot";
+
+        public decimal Price => 2.8m;
+    }
+
+    abstract class Deco : IComponent
+    {
+        protected readonly IComponent parent;
+
+        protected Deco(IComponent parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract string Name { get; }
+
+        public abstract decimal Price { get; }
+    }
+
+    class Salami : Deco
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Salami";
+
+        public override decimal Price => parent.Price + 3.2m;
+    }
+
+    class Käse : Deco
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Käse";
+
+        public override decimal Price => parent.Price + 2.1m;
+    }
+
+}
---
+++ DecoPattern/DecoPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DecoPattern;
+
+Console.WriteLine("Hello, World!");
+
+
+var brot = new Käse(new Salami(new Salami(new Brot())));
+
+
+Console.WriteLine($"{brot.Name} {brot.Price}");
+
+var json = System.Text.Json.JsonSerializer.Serialize(brot);
+Console.WriteLine(json);
