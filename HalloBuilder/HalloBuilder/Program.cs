1f624a80a252483cb1da30a54da2a47f68169b08
André <AndreR@ppedv.de>
2024-09-23 11:58:51 +0200

    Add initial project setup with builder pattern example
    Introduced a new Visual Studio solution file `HalloBuilder.sln` with configurations for Debug and Release modes. Added a new project file `HalloBuilder.csproj` targeting .NET 8.0, enabling implicit usings and nullable reference types.
    
    Added `Program.cs` with a console application that demonstrates the builder pattern by creating instances of the `Schrank` class and printing "Hello, World!" to the console.
    
    Added `Schrank.cs` defining the `Schrank` class with properties for `AnzahlTüren`, `Farbe`, and `Oberfläche`, including a nested `Builder` class and an `Oberfläche` enum for surface finishes.

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,6 @@
+﻿using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+var schrank1 = new Schrank.Builder().SetTüren(2).SetOberfläche(Oberfläche.Lackiert).SetFarbe("Grün").Build();
+var schrank2 = new Schrank.Builder().SetTüren(2).SetFarbe("Blau").Build();
+Console.ReadLine();
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,52 @@
+﻿
+namespace HalloBuilder
+{
+    internal class Schrank
+    {
+        public int AnzahlTüren { get; private set; }
+        public string Farbe { get; private set; } = string.Empty;
+        public Oberfläche Oberfläche { get; private set; }
+
+        internal class Builder
+        {
+            private Schrank _schrank = new Schrank();
+
+            public Builder SetTüren(int anzahlTüren)
+            {
+                if (anzahlTüren < 2 || anzahlTüren > 6)
+                    throw new System.ArgumentException("Anzahl der Türen muss zwischen 2 und 6 liegen.");
+
+                _schrank.AnzahlTüren = anzahlTüren;
+                return this;
+            }
+
+            public Builder SetFarbe(string farbe)
+            {
+                if(_schrank.Oberfläche != Oberfläche.Lackiert)
+                    throw new System.ArgumentException("Farbe kann nur bei lackierten Schränken gesetzt werden.");
+                _schrank.Farbe = farbe;
+                return this;
+            }
+            
+            public Builder SetOberfläche(Oberfläche oberfläche)
+            {
+                _schrank.Oberfläche = oberfläche;
+                return this;
+            }   
+
+
+            public Schrank Build()
+            {
+                return _schrank;
+            }
+        }
+    }
+
+    public enum Oberfläche
+    {
+        Natur,
+        Lackiert,
+        Geölt,
+        Gewachst
+    }
+}
