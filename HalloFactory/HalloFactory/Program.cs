7b134ffe76332be686f6b90c018e6ab0b915e087
André <AndreR@ppedv.de>
2024-09-23 11:37:24 +0200

    Add initial solution and project setup for HalloFactory
    Created a new Visual Studio solution file `HalloFactory.sln` targeting Visual Studio Version 17, including a project named "HalloFactory" with Debug and Release configurations. Added `HalloFactory.csproj` specifying .NET 8.0 framework and package references for `Microsoft.Data.SqlClient` (v5.2.0) and `Microsoft.Data.Sqlite` (v8.0.3). Implemented a simple console application in `Program.cs` that connects to either a SQL Server or SQLite database based on the day of the week, executes a SQL command to count employees, and prints the result.

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+
+Console.WriteLine("Hello, World!");
+
+string conStringSQL = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true";
+string conStringSQLITE = @"Data Source=C:\DB\Northwind.sqlite";
+
+DbProviderFactory factory = null;
+string conString = "";
+if (DateTime.Now.DayOfWeek != DayOfWeek.Wednesday)
+{
+    factory = Microsoft.Data.SqlClient.SqlClientFactory.Instance;
+    conString = conStringSQL;
+}
+else
+{
+    factory = Microsoft.Data.Sqlite.SqliteFactory.Instance;
+    conString = conStringSQLITE;
+}
+
+DbConnection con = factory.CreateConnection();
+con.ConnectionString = conString;
+con.Open();
+Console.WriteLine("Db open");
+
+DbCommand cmd = factory.CreateCommand();
+cmd.Connection = con;
+cmd.CommandText = "SELECT COUNT(*) FROM Employees";
+
+var rowCount = cmd.ExecuteScalar();
+
+Console.WriteLine($"Employees: {rowCount}");
