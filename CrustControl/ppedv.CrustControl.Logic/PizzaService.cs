b98ede0cc407c04932f7cc7e30228ff38defb7fe
André <AndreR@ppedv.de>
2024-09-25 11:45:37 +0200

    Add new project `ppedv.CrustControl.Logic` with `PizzaService`
    A new project named `ppedv.CrustControl.Logic` has been added to the solution `CrustControl.sln`. The project file targets .NET 8.0, enables implicit usings and nullable reference types, and includes a reference to `ppedv.CrustControl.Model`.
    
    The build configurations for Debug and Release modes have been added to the solution's global section.
    
    A new class `PizzaService` has been introduced in the `ppedv.CrustControl.Logic` namespace with the following methods:
    - `GetMostOrderdPizzaOfMonth`: Throws `NotImplementedException`.
    - `IsVegan`: Returns true if all toppings are vegan and vegetarian.
    - `IsVegetarian`: Returns true if all toppings are vegetarian.

---
+++ CrustControl/ppedv.CrustControl.Logic/PizzaService.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.CrustControl.Model;
+
+namespace ppedv.CrustControl.Logic
+{
+    public class PizzaService
+    {
+
+        public Pizza GetMostOrderdPizzaOfMonth(int month)
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(month, 12, nameof(month));
+            ArgumentOutOfRangeException.ThrowIfLessThan(month, 1, nameof(month));
+
+            throw new NotImplementedException();
+        }
+
+        public bool IsVegan(Pizza pizza)
+        {
+            return pizza.Toppings.All(x => x.Vegan && x.Vegetarian);
+        }
+
+        public bool IsVegetarian(Pizza pizza)
+        {
+            return pizza.Toppings.All(x => x.Vegetarian);
+        }
+    }
+}
