656b3954adb1a692c1f7a1c5ab0ae24fc26765e3
André <AndreR@ppedv.de>
2024-09-30 12:39:33 +0200

    Add new WPF project and implement pizza management UI
    * Added a new project `ppedv.CrustControl.UI.Desktop` to the solution `CrustControl.sln`.
    * Updated the solution configuration to include build configurations for the new project `ppedv.CrustControl.UI.Desktop`.
    * Added a new XML file `xmlFürNüchterne.xml` with two `Bank` elements, each with different namespaces and properties.
    * Created `App.xaml` and `App.xaml.cs` for the new WPF application `ppedv.CrustControl.UI.Desktop`.
    * Added `AssemblyInfo.cs` with theme information for the WPF application.
    * Created `MainWindow.xaml` and `MainWindow.xaml.cs` for the main window of the WPF application, including a menu, status bar, and tab control.
    * Added a new project file `ppedv.CrustControl.UI.Desktop.csproj` for the WPF application, targeting .NET 8.0 and enabling WPF.
    * Implemented `PizzaViewModel.cs` with properties and commands for managing a list of pizzas, including a special price calculation and a save command.
    * Created `HalloBindingView.xaml` and `HalloBindingView.xaml.cs` for a user control demonstrating data binding.
    * Created `PizzaView.xaml` and `PizzaView.xaml.cs` for a user control to display and manage pizzas, including data binding to `PizzaViewModel`.

---
+++ CrustControl/ppedv.CrustControl.UI.Desktop/ViewModels/PizzaViewModel.cs
@@ -0,0 +1,80 @@
+﻿using ppedv.CrustControl.Model.Contracts;
+using ppedv.CrustControl.Model.DomainModel;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Input;
+
+namespace ppedv.CrustControl.UI.Desktop.ViewModels
+{
+    class PizzaViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Pizza> PizzaList { get; set; }
+
+        private Pizza _selectedPizza;
+
+        public Pizza SelectedPizza
+        {
+            get => _selectedPizza;
+            set
+            {
+                _selectedPizza = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedPizza)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PizzaSpecialPrice)));
+            }
+        }
+
+        public string PizzaSpecialPrice
+        {
+            get
+            {
+                if (SelectedPizza == null)
+                    return "---";
+
+                return (SelectedPizza.Price / 2).ToString("C");
+            }
+        }
+
+        public ICommand SaveCommand { get; set; }
+
+        private readonly IRepository repo;
+
+        public PizzaViewModel(IRepository repo)
+        {
+            this.repo = repo;
+            SaveCommand = new SaveCommand(repo);
+
+            PizzaList = new ObservableCollection<Pizza>(repo.Query<Pizza>());
+        }
+
+        //pfusch bis DI Framework da ist
+        static string conString = "Server=(localdb)\\mssqllocaldb;Database=CrustControl_TestDb;Trusted_Connection=true";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public PizzaViewModel() : this(new Data.Db.EfContextRepositoryAdapter(conString))
+        { }
+    }
+
+    class SaveCommand : ICommand
+    {
+        private readonly IRepository repo;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            repo.SaveAll();
+        }
+
+        public SaveCommand(IRepository repo)
+        {
+            this.repo = repo;
+        }
+    }
+}
---
+++ CrustControl/ppedv.CrustControl.UI.Desktop/Views/PizzaView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ppedv.CrustControl.UI.Desktop.Views
+{
+    /// <summary>
+    /// Interaction logic for PizzaView.xaml
+    /// </summary>
+    public partial class PizzaView : UserControl
+    {
+        public PizzaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
