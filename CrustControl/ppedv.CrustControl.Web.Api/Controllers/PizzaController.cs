0e82e3f5e5746c81a482ed27bf185b47eaf135ad
André <AndreR@ppedv.de>
2024-10-01 12:29:15 +0200

    Kaputte WebAPI

---
+++ CrustControl/ppedv.CrustControl.Web.Api/Controllers/PizzaController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ppedv.CrustControl.Model.Contracts;
+using ppedv.CrustControl.Model.DomainModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ppedv.CrustControl.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private readonly IRepository repo;
+
+        public PizzaController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: api/<PizzaController>
+        [HttpGet]
+        public IEnumerable<Pizza> Get()
+        {
+            return repo.Query<Pizza>().ToList();
+        }
+
+        // GET api/<PizzaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PizzaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PizzaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PizzaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CrustControl/ppedv.CrustControl.Web.Api/Program.cs
@@ -0,0 +1,34 @@
+using ppedv.CrustControl.Data.Db;
+using ppedv.CrustControl.Logic;
+using ppedv.CrustControl.Model.Contracts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=CrustControl_TestDb;Trusted_Connection=true";
+
+builder.Services.AddSingleton<PizzaService>();
+builder.Services.AddTransient<IRepository>(x => new EfContextRepositoryAdapter(conString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
