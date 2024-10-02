01427901f39aa45f023cb84b68ecbeecc0e6675f
André <AndreR@ppedv.de>
2024-10-02 10:07:46 +0200

    ASP Web API mit DTO

--- CrustControl/ppedv.CrustControl.Web.Api/Controllers/PizzaController.cs
+++ CrustControl/ppedv.CrustControl.Web.Api/Controllers/PizzaController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ppedv.CrustControl.Model.Contracts;
 using ppedv.CrustControl.Model.DomainModel;
+using ppedv.CrustControl.Web.Api.Mapper;
+using ppedv.CrustControl.Web.Api.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +13,7 @@ namespace ppedv.CrustControl.Web.Api.Controllers
     public class PizzaController : ControllerBase
     {
         private readonly IRepository repo;
+        private readonly PizzaMapper mapper = new PizzaMapper();
 
         public PizzaController(IRepository repo)
         {
@@ -19,34 +22,59 @@ namespace ppedv.CrustControl.Web.Api.Controllers
 
         // GET: api/<PizzaController>
         [HttpGet]
-        public IEnumerable<Pizza> Get()
+        public IEnumerable<PizzaDTO> Get()
         {
-            return repo.Query<Pizza>().ToList();
+            foreach (var item in repo.Query<Pizza>().ToList())
+            {
+                yield return mapper.MapToDTO(item);
+            }
         }
 
         // GET api/<PizzaController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<PizzaDTO> Get(int id)
         {
-            return "value";
+            var pizza = repo.GetById<Pizza>(id);
+
+            if (pizza == null)
+                return NotFound();
+                
+            return mapper.MapToDTO(pizza);
         }
 
         // POST api/<PizzaController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] PizzaDTO value)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var pizza = mapper.MapToEntity(value);
+            repo.Add(pizza);
+            repo.SaveAll();
         }
 
         // PUT api/<PizzaController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] PizzaDTO value)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var pizza = mapper.MapToEntity(value);
+            repo.Update(pizza);
+            repo.SaveAll();
         }
 
         // DELETE api/<PizzaController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var pizza = repo.GetById<Pizza>(id);
+            if (pizza == null)
+                return NotFound($"Pizza wir ID {id} wurde nicht gefunden.");
+            repo.Delete(pizza);
+            repo.SaveAll();
+
+            return Ok();
         }
     }
 }
---
+++ CrustControl/ppedv.CrustControl.Web.Api/Mapper/PizzaMapper.cs
@@ -0,0 +1,30 @@
+﻿using ppedv.CrustControl.Model.DomainModel;
+using ppedv.CrustControl.Web.Api.Model;
+
+namespace ppedv.CrustControl.Web.Api.Mapper
+{
+    public class PizzaMapper
+    {
+        public PizzaDTO MapToDTO(Pizza pizza)
+        {
+            return new PizzaDTO
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                Price = pizza.Price,
+                Toppings = pizza.Toppings.Select(t => t.Name).ToArray()
+            };
+        }
+
+        public Pizza MapToEntity(PizzaDTO pizzaDTO)
+        {
+            return new Pizza
+            {
+                Id = pizzaDTO.Id,
+                Name = pizzaDTO.Name,
+                Price = pizzaDTO.Price,
+                Toppings = pizzaDTO.Toppings.Select(t => new Topping { Name = t }).ToList()
+            };
+        }
+    }
+}
